90b30e31fac5a741391f1c308530560a77dd1376
barto2014 <bbartito8@gmail.com>
2026-09-12 00:19:07 -0300

    up

--- android-tv/app/src/main/java/com/bartotv/tv/Config.java
+++ android-tv/app/src/main/java/com/bartotv/tv/Config.java
@@ -4,7 +4,7 @@ package com.bartotv.tv;
 // Cambiá OWNER/REPO si movés los datos al repo oculto.
 public final class Config {
     public static final String OWNER = "barto2014";
-    public static final String REPO = "nein";
+    public static final String REPO = "qzv-9f3k7p2q8x1z44m";
     public static final String BRANCH = "main";
     public static final String PATH = "datar.bin";
 
